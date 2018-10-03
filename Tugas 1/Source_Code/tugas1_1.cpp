6eaa4b2c5cf0323c4cb7d7fe57184e10d1d0beed
rusagaib <33116863+rusagaib@users.noreply.github.com>
2018-10-03 23:03:13 +0700

    Update tugas1_1.cpp

--- Tugas 1/Source_Code/tugas1_1.cpp
+++ Tugas 1/Source_Code/tugas1_1.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <conio.h>
 using namespace std;
-
+int n;
 int main(){
-	int n;
-	float x[n],jml=0,rata2;
+	 int x[n];
+	float rata,jml=0;
 	cout<<"masukkan Jumlah element= ";cin>>n;
-	for (float i=0; i<n;i++){
+	for (int i=0; i<n; i++){
 		cout<<"masukkan nilai ke-"<<i+1<<" = ";cin>>x[n];
 		jml+=x[n];
 	}
-	rata2=jml/n;
-	cout<<"Jumlah rata-rata adalah= "<<rata2<<endl;
-getch();
+	rata=jml/n;
+	cout<<"Rata-Ratanya adalah= "<<rata<<endl;
 }
